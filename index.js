108efa5717e3d48abfa1941a401965ae2644d0aa
bharathreddza <bharathreddyza@gmail.com>
2022-09-19 20:01:10 +0530

    reupload

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ import videoRoutes from "./routes/videos.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
-
+import cors  from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000","https://youtube-frontend-beta.vercel.app/"],
+    credentials: true,
+  })
+);
 dotenv.config();
 
 const connect = () => {
@@ -21,6 +28,7 @@ const connect = () => {
       throw err;
     });
 };
+
 
 //middlewares
 app.use(cookieParser())
