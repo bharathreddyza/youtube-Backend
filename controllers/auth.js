d482fe17552048452fb892852365a309009e5264
bharathreddza <bharathreddyza@gmail.com>
2022-09-21 14:40:29 +0530

    modify

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,7 +12,16 @@ export const signup = async (req, res, next) => {
     const newUser = new User({ ...req.body, password: hash });
 
     await newUser.save();
-    res.status(200).send("User has been created!");
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT);
+    const { password, ...others } = newUser._doc;
+
+    // res.status(200).send("User has been created!");
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
   } catch (err) {
     console.log(err)
     next(err);
